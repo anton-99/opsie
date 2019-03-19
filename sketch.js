e7612dc0712ce2d4bc50fed9c57cab3ac115818a
anton-99 <46093137+anton-99@users.noreply.github.com>
2019-03-19 16:02:18 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,31 @@
 function setup() {
   // put setup code here
+  createCanvas(1000, 1000);
 }
+var x=90;
+var y= 90;
 
 function draw() {
   // put drawing code here
+  background(100, 200, 200,30);
+  var objekt = ellipse(mouseX,mouseY,x,y);
+  fill(100);
+
+
+
+
 }
+
+
+function mouseDragged() {
+  x = x + 5;
+  y = y + 5;
+  if (x > 400 || y>400) {
+    x = 0;
+    y = 0;
+  }
+}
+
+
+
+console.log('test');
