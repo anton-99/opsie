f51b42fd62fc243d63452f28693d6a1d7491aadf
anton-99 <46093137+anton-99@users.noreply.github.com>
2019-03-19 16:03:00 +0100

    Update jojo.js

--- jojo.js
+++ jojo.js
@@ -1,4 +1,4 @@
-var eins= 6; // kein datentyp deklarieren 
+var eins= 6; // kein datentyp deklarieren
 var zwei =' Mein Name '; // string
 //namenkonevention erste wort klein danchch groß z.b vorName oder nachNameZwei
 // let auch datentyp für variable z.b let eins = 6; aber kleine unterschiede siehe https://stackoverflow.com/questions/762011/whats-the-difference-between-using-let-and-var-to-declare-a-variable-in-jav
@@ -7,9 +7,9 @@ console.log('hallo'); // im terminal printen
 document.write('<br>'+'Alui Haroun stinkt');
 document.write('<br>'+'for schleife '+'<br>');
 
-for ( i = 0; i < 5; i++) {
-  document.write(eins + i + "<br>"); // <br> ist enter bzw. newline 
+for ( i = 0; i < 16; i++) {
+  document.write(eins + i + "<br>"); // <br> ist enter bzw. newline
 }
-document.write(eins);// im browser anzeigen 
+document.write(eins);// im browser anzeigen
 document.write(zwei);
 //alert(eins);
