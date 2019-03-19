8fe16ceb877e047175304bc104fdcdf2b80f4971
anton-99 <46093137+anton-99@users.noreply.github.com>
2019-03-19 15:58:56 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Road 2 Goat</title>
+  </head>
+  <body>
+    <script>
+    var eins= 6;
+    for ( i = 0; i < 9; i++) {
+      document.write(eins + i + "<br>");
+    }
+    document.write(eins);
+    alert(eins);
+    </script>
+  </body>
+</html>
